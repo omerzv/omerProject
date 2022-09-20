98e5460dfdef04eb5e8e5a6623e073600a76a19a
User <User@DESKTOP-PTF7204>
2022-09-20 05:59:10 +0300

    b

--- OOPGame/src/Manu/Help.java
+++ OOPGame/src/Manu/Help.java
@@ -37,10 +37,10 @@ public class Help extends FrameFactory   {
 		g2d.drawString("The goal of this game is to survive as long as you can.", 50, 70);
 		g2d.drawString("If any of the enemy touching you, your health will get down.", 50, 100);
 		g2d.drawString("You are the white player,and you can move with the arrows.", 50, 150);
-		g2d.drawString("Pressing space will triger a dodge and your player become gray,", 50, 180);
+		g2d.drawString("Pressing space will trigger a dodge and your player become gray,", 50, 180);
 		g2d.drawString("in this state you dont get any damage for less then a second.", 50, 210);
-		g2d.drawString("There are green apple that spwan around the screen,", 50, 260);
-		g2d.drawString("only when you eat one the other will spwan.", 50, 290);
+		g2d.drawString("There are green apple that spawn around the screen,", 50, 260);
+		g2d.drawString("only when you eat one the other will spawn.", 50, 290);
 		g2d.drawString("Every apple you eat will rise your level,", 50, 320);
 		g2d.drawString("every 10 levels you will get faster and more durable to touching.", 50, 350);
 		g2d.drawString("There are also blue apples.", 50, 400);
